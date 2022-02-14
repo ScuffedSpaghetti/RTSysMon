10eed413074a206d34c1779ff6c2e960205bf57f
JordanSchlick <91904908+JordanSchlick@users.noreply.github.com>
2022-02-14 15:18:09 -0600

    Editing strings

--- client/devices/cpu-linux.js
+++ client/devices/cpu-linux.js
@@ -101,7 +101,7 @@ module.exports = class LinuxCPU extends GenericCPU{
 				}
 				if(process.env.VERBOSE){
 					console.error(err)
-					console.error("Could not get cpu power. Root privileges are needed to access msr and turbostat must be installed")
+					console.error("Could not get cpu power. Root privileges are needed to access MSR and turbostat must be installed")
 				}
 			}
 		}

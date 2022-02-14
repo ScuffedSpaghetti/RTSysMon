010dc72ff6f6b263b8561cf3aa4aa65cb6d1924f
JordanSchlick <91904908+JordanSchlick@users.noreply.github.com>
2022-02-14 16:27:09 -0600

    Fixed for vue 2 electric boogalo

--- server/webapp/main.js
+++ server/webapp/main.js
@@ -1,13 +1,17 @@
 //@ts-check
 
-import { createApp } from "vue/dist/vue.runtime.esm-bundler"
+// import { createApp } from "vue/dist/vue.runtime.esm-bundler"
+import Vue from "vue"
 
 //@ts-ignore
 import App from "./App.vue"
 // import Donut from 'vue-css-donut-chart'
 
-console.log(createApp)
+//console.log(createApp)
 
-var app = createApp(App)
-app.mount("#app")
+var app = new Vue(App)
+
+
+
+app.$mount("#app")
 // app.use(Donut)

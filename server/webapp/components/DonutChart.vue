97f9797073ad8fb3960df0ac1b103fe29402d666
Quinten Robley <robleyq@msoe.edu>
2022-02-14 15:18:07 -0600

    scuffed code v1.0.1

--- server/webapp/components/DonutChart.vue
+++ server/webapp/components/DonutChart.vue
@@ -1,29 +1,23 @@
 <template>
-    <div id="app">
-         <ejs-accumulationchart id="container">
-            <e-accumulation-series-collection>
-                <e-accumulation-series :dataSource='seriesData' type='Pie' xName='x' yName='y'> </e-accumulation-series>
-            </e-accumulation-series-collection>
-        </ejs-accumulationchart>
-    </div>
+  <vc-donut :sections="sections">Basic donut</vc-donut>
 </template>
+
 <script>
-import { AccumulationChartPlugin, PieSeries } from "@syncfusion/ej2-charts";
+  import Donut from 'vue-css-donut-chart';
+  import 'vue-css-donut-chart/dist/vcdonut.css';
 
-Vue.use(AccumulationChartPlugin);
-
-export default {
-  data() {
-    return {
-      seriesData: [
-            { x: 'United States', y: 45, text: 'USA', fill: '#00226C' },
-            { x: 'Australia', y: 53, text: 'AUS: 14%', fill: '#0450C2' },
-            { x: 'China', y: 56, text: 'CHN', fill: '#0073DC' },
-            { x: 'India', y: 61, text: 'IND', fill: '#0D98FF' },
-            { x: 'Japan', y: 13, text: 'JPN', fill: '#9CD9FF' },
-            { x: 'United Kingdom', y: 71, text: 'UK', fill: '#0450C2' }
-        ],
-    };
-  }
-};
+  export default {
+    data() {
+      return {
+        sections: [
+          { label: 'Red section', value: 25, color: 'red' },
+          { label: 'Green section', value: 25, color: 'green' },
+          { label: 'Blue section', value: 25, color: 'blue' }
+        ]
+      };
+    },
+    components:{
+      Donut
+    }
+  };
 </script>

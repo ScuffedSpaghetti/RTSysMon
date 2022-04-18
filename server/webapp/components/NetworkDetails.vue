f05bcfd36533cd3289dae230f56ec41d2473fb7e
JordanSchlick <91904908+JordanSchlick@users.noreply.github.com>
2022-04-17 21:06:12 -0500

    Auto postfix for network

--- server/webapp/components/NetworkDetails.vue
+++ server/webapp/components/NetworkDetails.vue
@@ -7,12 +7,12 @@
 					<div  class="item" v-if="x.rx_bytes != undefined">
 						<div class="info-text">Receive</div>
 						<HorizontalBar class="flex-child" v-if="x.rx_usage != undefined" :usage="x.rx_usage" :width="compWidth" :height="compHeight"/>
-						<div class="info-text">{{toMb(x.rx_bytes)}}Mbps {{(x.rx_bytes_limit)?"/" + toMb(x.rx_bytes_limit) + "Mbps":""}}</div>
+						<div class="info-text">{{autoBits(x.rx_bytes)}}ps {{(x.rx_bytes_limit)?"/" + autoBits(x.rx_bytes_limit) + "ps":""}}</div>
 					</div>
 					<div  class="item" v-if="x.tx_bytes != undefined">
 						<div class="info-text">Transmit</div>
 						<HorizontalBar class="flex-child" v-if="x.tx_usage != undefined" :usage="x.tx_usage" :width="compWidth" :height="compHeight"/>
-						<div class="info-text">{{toMb(x.tx_bytes)}}Mbps {{(x.tx_bytes_limit)?"/" + toMb(x.tx_bytes_limit) + "Mbps":""}}</div>
+						<div class="info-text">{{autoBits(x.tx_bytes)}}ps {{(x.tx_bytes_limit)?"/" + autoBits(x.tx_bytes_limit) + "ps":""}}</div>
 					</div>
 				</div>
             </div>
@@ -54,6 +54,27 @@ export default{
         toMb(bytes){
 			return (bytes / 1000 / 1000 * 8).toFixed(1)
 		},
+		autoBits(bytes){
+			var bits = bytes * 8
+			var suffix = "b"
+			if(bits >= 1000){
+				bits /= 1000
+				suffix = "Kb"
+			}
+			if(bits >= 1000){
+				bits /= 1000
+				suffix = "Mb"
+			}
+			if(bits >= 1000){
+				bits /= 1000
+				suffix = "Gb"
+			}
+			if(bits >= 1000){
+				bits /= 1000
+				suffix = "Tb"
+			}
+			return bits.toFixed(1) + " " + suffix
+		}
     },
     computed:{
        individualData(){

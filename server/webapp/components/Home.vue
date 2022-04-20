1e23b4bd4ea625aea18485bb083beee343610df0
Quinten Robley <qprtechman@gmail.com>
2022-04-19 21:26:09 -0500

    Changed loading text to loading svg

--- server/webapp/components/Home.vue
+++ server/webapp/components/Home.vue
@@ -6,7 +6,9 @@
             </span>
             <h1 style="text-align:center" v-if="Object.keys(this.info.individual).length == 0">No System Nodes Connected</h1>
         </div>
-        <h2 style="text-align:center" v-else>Loading...</h2>
+        <div style="text-align:center" v-else>
+            <img style="margin:auto" src="../public/images/loadingDots.svg" alt="Three Loading Dots">
+        </div>
         <div class="container">
             <span class="link pad item" v-on:click='gotoNode(x.uid)' v-for="x in this.info.individual" :key="x.uid">
                 <SmallComputerOverview  :info="x" :compHeight="5" :compWidth="8" :compTitle="x.hostname"/>
